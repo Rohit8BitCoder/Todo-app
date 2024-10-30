c59b0c6468b23e883ee543e20359147002b59b30
Rohit Singh Negi <37205344+Rohit8BitCoder@users.noreply.github.com>
2024-10-30 20:50:07 +0530

    Update index.js

--- backend/api/index.js
+++ backend/api/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
-const { Todo } = require('../api/db'); // Adjust path as needed
-const { createTodo, updateTodo } = require("../api/types.js");
+const { Todo } = require('./api/db'); // Adjust path as needed
+const { createTodo, updateTodo } = require("./api/types.js");
 
 const app = express();
 app.use(express.json());
